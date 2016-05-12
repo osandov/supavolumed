764c6d2eb232b63c113c01f07f09e5af8ae6db63
Omar Sandoval <osandov@osandov.com>
2016-05-11 21:52:33 -0700

    Use -Wno-errors=pedantic to work around glib on GCC 6
    I'm getting the following error when compiling with GCC 6:
    
    /usr/include/glib-2.0/gobject/gparam.h:166:33: error: enumerator value for ‘G_PARAM_DEPRECATED’ is not an integer constant expression [-Werror=pedantic]
       G_PARAM_DEPRECATED          = 1 << 31
    
    This seems like a false positive, but let's unbreak the build.

--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
 PREFIX = /usr/local
-CFLAGS = -std=c99 -pedantic -Wall -Werror -D_DEFAULT_SOURCE -O2 `pkg-config --cflags gtk+-3.0 libnotify libpulse libpulse-mainloop-glib x11`
+CFLAGS = -std=c99 -pedantic -Wall -Werror -Wno-error=pedantic -D_DEFAULT_SOURCE -O2 `pkg-config --cflags gtk+-3.0 libnotify libpulse libpulse-mainloop-glib x11`
 LDFLAGS = `pkg-config --libs gtk+-3.0 libnotify libpulse libpulse-mainloop-glib x11`
